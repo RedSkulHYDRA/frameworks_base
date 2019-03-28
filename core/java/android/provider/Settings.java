a76d6753b57d29f8db96f278fbe700a147cfda51
Lucchetto <lucchetto.tie@live.com>
2019-03-28 18:41:03 +0100

    base: Smart Charging (1/4)
    Also includes:
    
      From: DennySPB <dennyspb@gmail.com>
      Date: Fri, 17 May 2019 09:49:23 +0300
      Subject: [PATCH] SmartCharging: Rework and add more customizations
    
      From: Pranav Vashi <neobuddy89@gmail.com>
      Date: Thu, 16 Jan 2020 01:30:38 +0530
      Subject: [PATCH] Add config to make Smart Charging available on device
    
      From: xNombre <kartapolska@gmail.com>
      Date: Thu, 19 Aug 2021 23:25:28 +0200
      Subject: [PATCH] SmartCharging: Make sure resume level is lower than suspend level
    
        Braindead settings fragment or end-user (you can choose) can set
        resume level higher than suspend level. This is obviously wrong.
    
        This causes Android to write suspend and resume commands very
        frequently and quickly, this can cause phone crashes due to
        improper locking in the kernel. Such issue was reported to me.
    
        I am definitely not fixing the setting tile so let's come up with
        an easier approach - not resuming charging if level is mistakenly
        set. This won't kill shitty qcom kernel any more.
    
        Bonus: simplified if condition
    
      Signed-off-by: xNombre <kartapolska@gmail.com>
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>
    
    base: Fix smart charging bug
    
    The value of mPowerInputSuspended is switched to true or false in the code, neglecting the fact that
    it can also be changed the sysfs node when the phone is plugged/unplugged.
    
    If the user unplugs when the power is above charging limit, mPowerInputSuspended is still true. Therefore,
    if it's plugged in again, there will be no attempt to suspend input. The phone will continue to charge.
    
    Fix by reading sysfs node for the actual value whenever updateSmartChargingStatus() is called.
    
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6184,8 +6184,39 @@ public final class Settings {
          * The order in each list decides the priority for each shortcut
          * @hide
          */
-        @Readable
-        public static final String KEYGUARD_QUICK_TOGGLES_NEW = "keyguard_quick_toggles_new";
+
+
+        /**
+         * @hide
+         */
+        public static final String SMART_CHARGING = "smart_charging";
+
+        /**
+         * @hide
+         */
+        public static final String SMART_CHARGING_RESET_STATS = "smart_charging_reset_stats";
+
+        /**
+         * @hide
+         */
+        public static final String SMART_CHARGING_LEVEL = "smart_charging_level";
+
+        /**
+         * @hide
+         */
+        public static final String SMART_CHARGING_RESUME_LEVEL = "smart_charging_resume_level";
+
+        /**
+         * Keys we no longer back up under the current schema, but want to continue to
+         * process when restoring historical backup datasets.
+         *
+         * All settings in {@link LEGACY_RESTORE_SETTINGS} array *must* have a non-null validator,
+         * otherwise they won't be restored.
+         *
+         * @hide
+         */
+        public static final String[] LEGACY_RESTORE_SETTINGS = {
+        };
 
         /**
          * These are all public system settings
