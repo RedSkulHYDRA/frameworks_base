eb03dca9500e611da33e1ac80d3b88dd9e6d763f
RedSkulHYDRA <ishitgupta11196@gmail.com>
2023-05-03 14:38:44 +0000

    Remove IdleManager (1/2)

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -131,7 +131,6 @@ import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.keyguard.KeyguardUpdateMonitorCallback;
 import com.android.keyguard.ViewMediatorCallback;
 import com.android.systemui.ActivityIntentHelper;
-import com.android.systemui.ArcaneIdleManager;
 import com.android.systemui.AutoReinflateContainer;
 import com.android.systemui.CoreStartable;
 import com.android.systemui.DejankUtils;
@@ -511,9 +510,6 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
     /** Controller for the Shade. */
     @VisibleForTesting
     NotificationPanelViewController mNotificationPanelViewController;
-
-   // Arcane Idle
-    private boolean isIdleManagerIstantiated = false;
 
     // settings
     private QSPanelController mQSPanelController;
@@ -3559,17 +3555,6 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
             }
 
             DejankUtils.stopDetectingBlockingIpcs(tag);
-            if (Settings.System.getIntForUser(mContext.getContentResolver(),
-                                              Settings.System.ARCANE_IDLE_MANAGER, 1,
-                                              mLockscreenUserManager.getCurrentUserId()) == 1) {
-                if (!isIdleManagerIstantiated) {
-                    ArcaneIdleManager.initManager(mContext);
-                    isIdleManagerIstantiated = true;
-                    ArcaneIdleManager.executeManager();
-                } else {
-                    ArcaneIdleManager.executeManager();
-                }
-            }
         }
 
         @Override
@@ -3600,11 +3585,6 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
 
             });
             DejankUtils.stopDetectingBlockingIpcs(tag);
-            if (Settings.System.getIntForUser(mContext.getContentResolver(),
-                                              Settings.System.ARCANE_IDLE_MANAGER, 1,
-                                              mLockscreenUserManager.getCurrentUserId()) == 1) {
-                ArcaneIdleManager.haltManager();
-            }
         }
 
         @Override
