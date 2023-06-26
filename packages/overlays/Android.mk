f1716f59e5cb43ffa6c07d2f57b249ce84847fc0
RedSkulHYDRA <ishitgupta11196@gmail.com>
2023-06-26 17:13:25 +0530

    IconPack*: Remove ThemePicker and Launcher overlays
    * Not used with S+ ThemePicker

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -27,40 +27,26 @@ LOCAL_REQUIRED_MODULES := \
 	DisplayCutoutEmulationWaterfallOverlay \
 	FontNotoSerifSourceOverlay \
 	IconPackCircularAndroidOverlay \
-	IconPackCircularLauncherOverlay \
 	IconPackCircularSettingsOverlay \
 	IconPackCircularSystemUIOverlay \
-	IconPackCircularThemePickerOverlay \
 	IconPackVictorAndroidOverlay \
-	IconPackVictorLauncherOverlay \
 	IconPackVictorSettingsOverlay \
 	IconPackVictorSystemUIOverlay \
-	IconPackVictorThemePickerOverlay \
 	IconPackSamAndroidOverlay \
-	IconPackSamLauncherOverlay \
 	IconPackSamSettingsOverlay \
 	IconPackSamSystemUIOverlay \
-	IconPackSamThemePickerOverlay \
 	IconPackKaiAndroidOverlay \
-	IconPackKaiLauncherOverlay \
 	IconPackKaiSettingsOverlay \
 	IconPackKaiSystemUIOverlay \
-	IconPackKaiThemePickerOverlay \
 	IconPackFilledAndroidOverlay \
-	IconPackFilledLauncherOverlay \
 	IconPackFilledSettingsOverlay \
 	IconPackFilledSystemUIOverlay \
-	IconPackFilledThemePickerOverlay \
 	IconPackRoundedAndroidOverlay \
-	IconPackRoundedLauncherOverlay \
 	IconPackRoundedSettingsOverlay \
 	IconPackRoundedSystemUIOverlay \
-	IconPackRoundedThemePickerOverlay \
 	IconPackOOSAndroidOverlay \
-	IconPackOOSLauncherOverlay \
 	IconPackOOSSettingsOverlay \
 	IconPackOOSSystemUIOverlay \
-	IconPackOOSThemePickerOverlay \
 	NavigationBarMode3ButtonOverlay \
 	NavigationBarModeGesturalOverlay \
 	NavigationBarModeGesturalOverlayNarrowBack \
